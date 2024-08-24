2b427a76d9d1f27ef53e7a4ac5542e57b35c18bd
ranajadev <jonathanaugustt2021@outlook.com>
2024-08-24 17:15:49 +0100

    Added

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1724515254|5955428766';
+const CACHE_VERSION = '1724516089|24441497';
 /** @type {string} */
 const CACHE_PREFIX = 'Ranaja Therapuet-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
